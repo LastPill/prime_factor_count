91eee7b5e5848d57a397b8adf07b535272038fad
LastPill <88427086+LastPill@users.noreply.github.com>
2021-08-04 22:00:48 +0300

    Update main2.py

--- main2.py
+++ main2.py
@@ -22,33 +22,3 @@ def sum_for_list(lst):
                 k += 1
     return ans
 
-
-
-
-
-    #         list = []
-    #         for k in lst:
-    #             if k%i == 0:
-    #                 list.append(k)
-    #         if len(list) != 0:
-    #             all_list.append([i, sum(list)])
-    # return all_list
-
-print(sum_for_list([200000, 2100880, 2408800, 3088000, 4500880]))
-
-# def natural(k):
-#     if k % 2 == 0:
-#         return k == 2
-#     d = 3
-#     while d * d <= k and k % d != 0:
-#         d += 2
-#     return d * d > k
-
-# def natural_list(p):
-#     list_n = [2]
-#     for i in range(3, abs(min(p)), 2):
-#         if natural(i) == True:
-#             list_n.append(i)
-#     return list_n
-
-
